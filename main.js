ed15b82f377beee1e33c98a2509a833338f4ac6c
Yago-Gil-Nunes <gilnunesyago@gmail.com>
2024-07-23 12:25:29 -0300

    add aprojeto github

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
-    /*
-    console.log(document.querySelector("header button"));
-    console.log($("#botao-cancelar"))
-
-    document.querySelector("header button").addEventListener("click", function(e){
-
-    })
-*/ 
     $("header button").click(function () {
-       $("form").slideDown();
+        $("form").slideDown();
+    })
+
+    $("#botao-cancelar").click(function(){
+        $("form").slideUp();
     })
 
     $("form").on("submit",function(e) {
-        console.log("submit");
         e.preventDefault();
-    });
-});
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display : none" ></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a hrfe="${enderecoDaNovaImagem}" target="_blank" title= "Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(3000)
+        $('#endereco-imagem-nova').val('');
+    })
+})
 
